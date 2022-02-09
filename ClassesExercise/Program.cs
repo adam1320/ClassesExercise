02a091a00410485dd4059dad10a495bea66d05f9
Adam Brannon <adam1320@rocketmail.com>
2022-02-09 12:49:12 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+
+            car.Make = "Dodge";
+
+            car.Model = "Challenger";
+
+            car.Year = 2013;
+            
+             Console.WriteLine($"{car.Make} {car.Model} {car.Year}") ;
+
+
+
+
+        }
+
+    }       
+            
+}
+
